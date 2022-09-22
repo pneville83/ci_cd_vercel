ff6796cbf402cf64533b389c551ea0a00db725e3
Peter Neville <neville.peter.2000@gmail.com>
2022-09-22 15:30:46 -0500

    cambios generales al proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+const [like, setLike] = useState (25);
+const [like2, setLike2] = useState (0);
+const [like3, setLike3] = useState (10);
+
+function likes (){
+  setLike ((prevLike)=> {
+    return prevLike ? prevLike -1 : 0;
+  });
+}
+
+function likes2 (){
+  setLike2 ((prevLike2)=> {
+    return prevLike2 ? prevLike2 -1 : 0;
+  });
+}
+
+function likes3 (){
+  setLike3 ((prevLike3)=> {
+    return prevLike3 ? prevLike3 -1 : 0;
+  });
+}
+
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p data-testid="countLike1">{like}</p>
+      <button data-testid="plus" onClick={()=> setLike ((prevLike)=> prevLike + 1)} >1 Like +</button>
+      <button data-testid="less"  onClick={()=> likes()} > 1 like - </button>
+
+      <p data-testid="countLike2">{like2}</p>
+      <button data-testid="plus2" onClick={()=> setLike2 ((prevlike2)=> prevlike2 + 1)} >1 Like +</button>
+      <button data-testid="less2"  onClick={()=> likes2()} > 1 like - </button>
+
+      <p data-testid="countLike3">{like3}</p>
+      <button data-testid="plus3" onClick={()=> setLike3 ((prevLike3)=> prevLike3 + 1)} >1 Like +</button>
+      <button data-testid="less3"  onClick={()=> likes3()} > 1 like - </button>
+
+
     </div>
-  )
+    
+  );
 }
 
-export default App
+export default App;
