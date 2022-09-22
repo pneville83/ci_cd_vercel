5196172a7d5d9677d1738e1bc9f8c789296eeaca
Peter Neville <neville.peter.2000@gmail.com>
2022-09-22 14:04:43 -0500

    subiendo proyecto

---
+++ src/__test__/app.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from '@testing-library/react';
+import App from '../App';
+
+
+test('Renders main page correctly', () => {
+  render(<App />)
+  expect(true).toBeTruthy();
+});
+
+
